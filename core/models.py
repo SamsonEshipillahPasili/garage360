040d5f7e6f55609682deee18eddcd644da0b7d9c
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2026-07-24 02:17:18 +0300

    Add the core.Client model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class TimestampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Client(TimestampedModel):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    email = models.EmailField()
