fd8fc002e4d4b0c725ab2c0a525d11dd21f21c2d
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2026-07-24 02:30:22 +0300

    Add the core.Vehicle model

--- core/models.py
+++ core/models.py
@@ -13,3 +13,10 @@ class Client(TimestampedModel):
     last_name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=255)
     email = models.EmailField()
+
+class Vehicle(TimestampedModel):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    brand = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    yom = models.PositiveIntegerField()
+    number_plate = models.CharField(max_length=255)
