a90bfa34e652382dbad24d7199327034442c99ae
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2026-07-28 00:40:19 +0300

    Add client manager method to add a new client. Also add Client.set_password to set client passwords for first time logins

--- core/models.py
+++ core/models.py
@@ -1,5 +1,10 @@
+import uuid
+
+import logzero
+
 from django.db import models
 from django.contrib.auth.models import User
+from django.db import transaction
 
 class TimestampedModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
@@ -8,9 +13,49 @@ class TimestampedModel(models.Model):
     class Meta:
         abstract = True
 
+class ClientManager(models.Manager):
+
+    def create_client(
+            self, email: str, first_name: str, last_name: str, phone: str
+    ) -> 'Client':
+        # open a transaction
+        with transaction.atomic():
+            # initial password
+            pwd = uuid.uuid4().hex
+
+            # first create the user
+            user = User.objects.create_user(
+                email=email,
+                password=pwd,
+                first_name=first_name,
+                last_name=last_name,
+                username=email,
+            )
+
+            # then create the client
+            return Client.objects.create(user=user, phone=phone)
+
+
 class Client(TimestampedModel):
     phone_number = models.CharField(max_length=255)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_password_set = models.BooleanField(default=False)
+
+    def set_password(self, password: str):
+        """
+        Set password for this client for first time logins.
+        Further attempts will throw an error.
+        """
+        if self.is_password_set:
+            raise ValueError(f"Password is already set for user: {self.user.username}")
+
+        with transaction.atomic():
+            self.user.set_password(password)
+            self.user.save(update_fields=["password"])
+            self.is_password_set = True
+            self.save()
+
+    objects = ClientManager()
 
 class Vehicle(TimestampedModel):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
