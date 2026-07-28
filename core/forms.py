398060cbb9923978851d0283dcb389aea1b0398d
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2026-07-28 21:29:26 +0300

    Removed the CreateClientForm.helper as it is no longer needed

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,3 @@
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit
 from django import forms
 
 class CreateClientForm(forms.Form):
@@ -7,16 +5,3 @@ class CreateClientForm(forms.Form):
     first_name = forms.CharField(max_length=255)
     last_name = forms.CharField(max_length=255)
     phone = forms.CharField(max_length=255)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.helper = FormHelper()
-
-        self.helper.layout = Layout(
-            "email",
-            "first_name",
-            "last_name",
-            "phone",
-            Submit("submit", "Create Client"),
-        )
