1658eb89b546681fa877f87056d4082954d24db1
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2026-07-29 14:36:59 +0300

    Added the test test_client_can_be_updated_correctly

--- core/tests.py
+++ core/tests.py
@@ -2,6 +2,18 @@ import pytest
 
 from core.models import Client
 from core.models import User
+from faker import Faker
+
+fake = Faker()
+
+@pytest.fixture
+def client_user(db):
+   return Client.objects.create_client(
+        email="client@gmail.com",
+        first_name="CFName",
+        last_name="CLName",
+        phone="+5757767676"
+    )
 
 @pytest.mark.django_db
 def test_can_correctly_create_client():
@@ -47,3 +59,29 @@ def test_client_password_is_set_correctly():
     client.set_password(new_password)
     assert client.is_password_set
     assert client.user.check_password(new_password)
+
+
+@pytest.mark.django_db
+def test_client_can_be_updated_correctly(client_user):
+    new_client_data = {
+        'email': fake.email(),
+        'first_name': fake.first_name(),
+        'last_name': fake.last_name(),
+        'phone': fake.phone_number(),
+    }
+
+    assert new_client_data['first_name'] != client_user.user.first_name
+    assert new_client_data['last_name'] != client_user.user.last_name
+    assert new_client_data['email'] != client_user.user.email
+    assert new_client_data['phone'] != client_user.phone_number
+
+    Client.objects.update_client(
+        instance=client_user,
+        **new_client_data
+    )
+
+    assert new_client_data['first_name'] == client_user.user.first_name
+    assert new_client_data['last_name'] == client_user.user.last_name
+    assert new_client_data['email'] == client_user.user.email
+    assert new_client_data['phone'] == client_user.phone_number
+
