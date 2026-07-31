3de958cebec6b09f6efb6cdb3e2ce849f9c52e51
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2026-07-31 13:40:47 +0300

    Added an email validator to the UserProfileForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.models import User
+
 
 class UserProfileForm(forms.Form):
     email = forms.EmailField()
@@ -6,3 +8,7 @@ class UserProfileForm(forms.Form):
     last_name = forms.CharField(max_length=255)
     phone_number = forms.CharField(max_length=255)
 
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email is taken!")
