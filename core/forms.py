af0dd8604a704be9c3381ca583bccfcd8bb33ff4
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2026-08-01 17:01:48 +0300

    Remove dead code

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,2 @@
 from django import forms
 
-class CreateClientForm(forms.Form):
-    email = forms.EmailField()
-    first_name = forms.CharField(max_length=255)
-    last_name = forms.CharField(max_length=255)
-    phone = forms.CharField(max_length=255)
