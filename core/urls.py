d0420e16c85828e74dc3b73774e61be82f400313
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2026-08-01 17:00:14 +0300

    Setup interim page for booking

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ app_name = 'core'
 
 urlpatterns = [
     path('index', views.IndexView.as_view(), name='index'),
+    path('booking/create/<int:profile_id>', views.CreateBookingView.as_view(), name='create_booking'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,25 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.http import HttpRequest
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+from django.views.generic import TemplateView, FormView
+
+from accounts.models import UserProfile
 
 
 class IndexView(LoginRequiredMixin, TemplateView):
     template_name = 'core/index.html'
+
+
+class CreateBookingView(LoginRequiredMixin, View):
+
+    def get(self, request: HttpRequest, profile_id: int):
+        qs = UserProfile.objects.select_related('user').filter(
+            organization=request.user.profile.organization,
+        )
+        user_profile = get_object_or_404(qs, pk=profile_id)
+
+        context = {
+            'profile': user_profile,
+        }
+        return render(request, 'core/create_booking.html', context)
