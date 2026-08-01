501d6ef00618bec4f19721bde9199172e6d67c6e
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2026-08-01 18:52:54 +0300

    Add the CreateBookingForm form

--- core/forms.py
+++ core/forms.py
@@ -1,2 +1,10 @@
 from django import forms
 
+class CreateBookingForm(forms.Form):
+    car_brand = forms.CharField(max_length=255)
+    car_model = forms.CharField(max_length=255)
+    car_number_plate = forms.CharField(max_length=255)
+    booking_date = forms.DateField(
+        widget=forms.DateInput(attrs={'type': 'date'}),
+    )
+
--- core/views.py
+++ core/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import QuerySet
 from django.http import HttpRequest
 from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView, FormView
+
+from django.views.generic import TemplateView
 
 from accounts.models import UserProfile
+from .forms import CreateBookingForm
 
 
 class IndexView(LoginRequiredMixin, TemplateView):
@@ -13,13 +16,24 @@ class IndexView(LoginRequiredMixin, TemplateView):
 
 class CreateBookingView(LoginRequiredMixin, View):
 
-    def get(self, request: HttpRequest, profile_id: int):
-        qs = UserProfile.objects.select_related('user').filter(
-            organization=request.user.profile.organization,
+    def _get_user_profile_qs(self, request: HttpRequest) -> QuerySet:
+        return (
+            UserProfile.objects
+                .select_related('user')
+                .filter(
+                    organization=request.user.profile.organization,
+                )
         )
+
+    def get(self, request: HttpRequest, profile_id: int):
+        qs = self._get_user_profile_qs(request=request)
         user_profile = get_object_or_404(qs, pk=profile_id)
+
+        booking_form = CreateBookingForm()
 
         context = {
             'profile': user_profile,
+            'booking_form': booking_form,
         }
         return render(request, 'core/create_booking.html', context)
+
