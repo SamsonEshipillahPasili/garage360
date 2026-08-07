ac526a30c40eb35684399d11590797d89801ba12
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2026-08-07 22:07:40 +0300

    Removed leading f-slash on some endpoints

--- core/urls.py
+++ core/urls.py
@@ -10,12 +10,12 @@ urlpatterns = [
     path('booking/all', views.ListAllBookingsView.as_view(), name='list_bookings'),
     path('booking/<int:booking_id>', views.BookingDetailView.as_view(), name='booking_detail'),
     path(
-        '/quotation/create_line/<int:quotation_id>',
+        'quotation/create_line/<int:quotation_id>',
         views.CreateQuotationItemView.as_view(),
         name='create_quotation_line'
     ),
     path(
-        '/quotation/delete_line/<int:line_id>',
+        'quotation/delete_line/<int:line_id>',
         views.DeleteQuotationLineView.as_view(),
          name='delete_quotation_line'
     ),
